d4d039758f6e0470f4515402c7ec88356c2b73a0
Andrew Klaas <andrewklaas@Andrews-MacBook-Pro.local>
2017-05-29 11:56:01 -0500

    update

--- terraform/modules/aws/compute/scripts/setup_mysql_vault.sh
+++ terraform/modules/aws/compute/scripts/setup_mysql_vault.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-sudo yum install -y mysql
 
 #step 1:
 if [ -z "$1" ]; then
 	echo "No arg/endpoint specified!"
 	exit
 fi
-
-export ROOT_TOKEN=$(consul kv get service/vault/root-token)
-export NOMAD_TOKEN=$(consul kv get service/vault/nomad-token)
-vault auth $ROOT_TOKEN
-vault write mysql/config/connection connection_url="$2:$3@tcp($MYSQL_IP:3306)/"
-vault write mysql/roles/app sql="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL PRIVILEGES ON app.* TO '{{name}}'@'%';"
-vault read mysql/creds/app
-echo "mysql ip:"  
-echo $MYSQL_IP
-echo "nomad-token"
-echo $NOMAD_TOKEN
-mysql -h $1 -u $2 -p$3 -e 'create database app;'
+if vault status | grep active > /dev/null; then
+	sudo yum install -y mysql
+	export ROOT_TOKEN=$(consul kv get service/vault/root-token)
+	export NOMAD_TOKEN=$(consul kv get service/vault/nomad-token)
+	vault auth $ROOT_TOKEN
+	vault write mysql/config/connection connection_url="$2:$3@tcp($MYSQL_IP:3306)/"
+	vault write mysql/roles/app sql="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL PRIVILEGES ON app.* TO '{{name}}'@'%';"
+	vault read mysql/creds/app
+	echo "mysql ip:"  
+	echo $MYSQL_IP
+	echo "nomad-token"
+	echo $NOMAD_TOKEN
+	mysql -h $1 -u $2 -p$3 -e 'create database app;'
+fi
 
 # Launch fabio and app
 # curl -H "Host: app.com" http://127.0.0.1:9999/
