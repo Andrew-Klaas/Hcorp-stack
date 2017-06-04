db5714fcba462f78290892293cab64dce628e1f6
Andrew Klaas <andrewklaas@Andrews-MacBook-Pro.local>
2017-06-04 17:29:57 -0500

    update

--- terraform/modules/aws/compute/scripts/setup_vault.sh
+++ terraform/modules/aws/compute/scripts/setup_vault.sh
@@ -9,6 +9,11 @@ echo "starting vault install"
 
 export VAULT_ADDR=http://127.0.0.1:8200
 
+# PKI specific variables
+RootCAName="vault-ca-root"
+IntermCAName="vault-ca-intermediate"
+mkdir -p /tmp/certs/
+
 cget() { curl -sf "http://127.0.0.1:8500/v1/kv/service/vault/$1?raw"; }
 
 if [ ! $(cget root-token) ]; then
@@ -65,7 +70,95 @@ EOF
   exit 0
 }
 
+policy_setup() {
+  logger "$0 - Configuring Vault Policies"
+  # create policy named 'waycoolapp'
+  echo "
+  path \"${IntermCAName}/issue*\" {
+    capabilities = [\"create\",\"update\"]
+  }
+  path \"secret/waycoolapp*\" {
+    capabilities = [\"read\"]
+  }
+  path \"auth/token/renew\" {
+    capabilities = [\"update\"]
+  }
+  path \"auth/token/renew-self\" {
+    capabilities = [\"update\"]
+  }
+  " | vault policy-write waycoolapp -
 
+  # create policy named 'admin-waycoolapp'
+  echo '
+  path "sys/mounts" {
+    capabilities = ["list","read"]
+  }
+  path "secret/*" {
+    capabilities = ["list", "read"]
+  }
+  path "secret/waycoolapp*" {
+    capabilities = ["create", "read", "update", "delete", "list"]
+  }
+  path "secret/aklaas" {
+    capabilities = ["create", "read", "update", "delete", "list"]
+  }
+  path "supersecret/*" {
+    capabilities = ["list", "read"]
+  }' | vault policy-write admin-waycoolapp -
+}
+
+admin_setup() {
+  logger "$0 - Configuring UserPass backend"
+  # setup userpass for a personal login
+  vault auth-enable userpass
+  # create my credentials
+  vault write auth/userpass/users/aklaas password=test policies="admin-waycoolapp"
+}
+
+pki_setup() {
+
+  # Mount Root CA and generate cert
+  vault unmount ${RootCAName} &> /dev/null || true
+  vault mount -path ${RootCAName} pki
+  vault mount-tune -max-lease-ttl=87600h ${RootCAName}
+  vault write -format=json ${RootCAName}/root/generate/internal \
+  common_name="${RootCAName}" ttl=87600h | tee >(jq -r .data.certificate > /tmp/certs/ca.pem) >(jq -r .data.issuing_ca > /tmp/certs/issuing_ca.pem) >(jq -r .data.private_key > /tmp/certs/ca-key.pem)
+
+  # Mount Intermediate and set cert
+  vault unmount ${IntermCAName} &> /dev/null || true
+  vault mount -path ${IntermCAName} pki
+  vault mount-tune -max-lease-ttl=87600h ${IntermCAName}
+  vault write -format=json ${IntermCAName}/intermediate/generate/internal common_name="${IntermCAName}" ttl=43800h | tee >(jq -r .data.csr > /tmp/certs/${IntermCAName}.csr) >(jq -r .data.private_key > /tmp/certs/${IntermCAName}.pem)
+
+  # Sign the intermediate certificate and set it
+  vault write -format=json ${RootCAName}/root/sign-intermediate csr=@/tmp/certs/${IntermCAName}.csr common_name="${IntermCAName}" ttl=43800h | tee >(jq -r .data.certificate > /tmp/certs/${IntermCAName}.pem) >(jq -r .data.issuing_ca > /tmp/certs/${IntermCAName}_issuing_ca.pem)
+  vault write ${IntermCAName}/intermediate/set-signed certificate=@/tmp/certs/${IntermCAName}.pem
+
+  # Generate the roles
+  vault write ${IntermCAName}/roles/example-dot-com allow_any_name=true max_ttl="1m"
+
+}
+
+approle_setup() {
+  logger "$0 - Configuring AppRole"
+
+  # enable approle backend
+  vault auth-enable approle
+  # create approle for 'waycoolapp' with above policy and approle specific parameters
+  vault write auth/approle/role/waycoolapp secret_id_num_uses=1000 period=3600 policies=waycoolapp
+  # read role_id for our approle
+  vault read auth/approle/role/waycoolapp/role-id | grep role_id | awk '{print $2}' > /tmp/role_id
+  # retrieve secret_id for our approle, and subsequently upload to consul for retrieval
+  # DEMO PURPOSES ONLY - NOT RECOMMENDED
+  vault write -format=json -f auth/approle/role/waycoolapp/secret-id | tee \
+  >(jq --raw-output '.data.secret_id' > /tmp/secret_id) \
+  >(jq --raw-output '.data.secret_id_accessor' > /tmp/secret_id_accessor)
+  curl -fX PUT 127.0.0.1:8500/v1/kv/service/vault/role_id -d $(cat /tmp/role_id)
+  sleep 2
+  curl -fX PUT 127.0.0.1:8500/v1/kv/service/vault/secret_id -d $(cat /tmp/secret_id)
+  sleep 2
+  curl -fX PUT 127.0.0.1:8500/v1/kv/service/vault/secret_id_accessor -d $(cat /tmp/secret_id_accessor)
+}
 
 if vault status | grep active > /dev/null; then
   # auth with root token
@@ -81,6 +174,13 @@ if vault status | grep active > /dev/null; then
     curl -fX PUT 127.0.0.1:8500/v1/kv/service/vault/nomad-token -d $NOMAD_TOKEN
     sudo echo $NOMAD_TOKEN > /tmp/test5
   fi
+
+  vault write secret/waycoolapp User1SSN="200-23-9930" User2SSN="000-00-0002" ttl=60s
+
+  policy_setup
+  admin_setup
+  pki_setup
+
 fi
 sudo echo "test6" > /tmp/test6
 #instructions
